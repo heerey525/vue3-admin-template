35cf0472537b4482795c91d52f92b67b9d9d36cb
heerey525 <190182347@qq.com>
2022-06-18 17:13:17 +0800

    feat(登录): 登录鉴权

---
+++ src/permission.js
@@ -0,0 +1,29 @@
+import router from '@/router'
+import store from '@/store'
+
+// 白名单
+const whiteList = ['/login']
+
+/**
+ * @description: 路由前置
+ * @return {*} to 即将要进入的目标
+ * @return {*} form 当前导航正要离开的路由
+ * @return {*} next 是否继续执行
+ */
+router.beforeEach((to, form, next) => {
+  // 用户已登录不允许进入login
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/layout')
+    } else {
+      next()
+    }
+  } else {
+    // 未登录则只允许进入login
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
