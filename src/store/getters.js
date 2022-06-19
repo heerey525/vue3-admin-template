661523d53fc9b98dcfecd54a2758b103a2e7ac77
heerey525 <190182347@qq.com>
2022-06-20 00:19:02 +0800

    feat(Navbar): 渲染用户头像菜单

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,9 @@ const getters = {
   token: (state) => state.user.token,
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo !== '{}')
+  },
+  userInfo: (state) => {
+    return state.user.userInfo
   }
 }
 
