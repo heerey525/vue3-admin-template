41de562bc2f385778d154c3f391e13996514b60d
heerey525 <190182347@qq.com>
2022-06-19 23:36:01 +0800

    feat(layout): 创建基础架构

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Sidebar 组件</div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>layout 页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+    <sidebar
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    ></sidebar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部 navbar -->
+        <navbar></navbar>
+      </div>
+      <!-- 内容区 -->
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar'
+import AppMain from './components/AppMain.vue'
+import variables from '@/styles/variables.scss'
 import {} from 'vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
