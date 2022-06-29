f9c8b2c5af31528ce7446c151ae2beb9fb4a66c6
heerey525 <190182347@qq.com>
2022-06-29 23:49:03 +0800

    feat(动态换肤): 处理element-plus主题变更

--- src/components/ThemePicker/components/SelectColor.vue
+++ src/components/ThemePicker/components/SelectColor.vue
@@ -21,6 +21,7 @@
 <script setup>
 import { defineProps, defineEmits, ref } from 'vue'
 import { useStore } from 'vuex'
+import { generateNewStyle, writeNewStyle } from '@/utils/theme'
 
 defineProps({
   modelValue: {
@@ -67,9 +68,9 @@ const closed = () => {
 
 const comfirm = async () => {
   // // 1.1 获取主题色
-  // const newStyleText = await generateNewStyle(mColor.value)
+  const newStyleText = await generateNewStyle(mColor.value)
   // // 1.2 写入最新主题色
-  // writeNewStyle(newStyleText)
+  writeNewStyle(newStyleText)
   // 2. 保存最新的主题色
   store.commit('theme/setMainColor', mColor.value)
   // 3. 关闭 dialog
