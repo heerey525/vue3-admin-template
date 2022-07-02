008428f06bbf509b52350d63f9c2e6ff56303265
heerey525 <190182347@qq.com>
2022-07-03 00:44:54 +0800

    feat(HeaderSearch): 数据源处理

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -27,15 +27,53 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import Fuse from 'fuse.js'
+import { filterRouters } from '@/utils/route'
+import { generateRoutes } from './FuseData'
+
+// 检索数据源
+const router = useRouter()
+const searchPool = () => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  return generateRoutes(filterRoutes)
+}
+const filterRoutes = filterRouters(router.getRoutes())
+console.log(generateRoutes(filterRoutes))
+
+/**
+ * 搜索库相关
+ */
+
+const fuse = new Fuse(searchPool(), {
+  // 是否按优先级进行排序
+  shouldSort: true,
+  // 匹配算法放弃的时机， 阈值 0.0 需要完美匹配（字母和位置），阈值 1.0 将匹配任何内容。
+  threshold: 0.4,
+  // 匹配长度超过这个值的才会被认为是匹配的
+  minMatchCharLength: 1,
+  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+  // name：搜索的键
+  // weight：对应的权重
+  keys: [
+    {
+      name: 'title',
+      weight: 0.7
+    },
+    {
+      name: 'path',
+      weight: 0.3
+    }
+  ]
+})
 
 // 控制 search 显示
 const isShow = ref(false)
 // el-select 实例
-// const headerSearchSelectRef = ref(null)
+const headerSearchSelectRef = ref(null)
 const onShowClick = () => {
-  console.log(1111, isShow)
   isShow.value = !isShow.value
-  // headerSearchSelectRef.value.focus()
+  headerSearchSelectRef.value.focus()
 }
 
 // search 相关
@@ -43,7 +81,9 @@ const search = ref('')
 // 搜索结果
 // const searchOptions = ref([])
 // 搜索方法
-const querySearch = (query) => {}
+const querySearch = (query) => {
+  console.log('搜索结果', fuse.search(query))
+}
 // 选中回调
 const onSelectChange = (val) => {}
 </script>
