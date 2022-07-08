f582433d1e6efa09bcfb3c245efef44be080974d
heerey525 <190182347@qq.com>
2022-07-08 23:48:44 +0800

    feat(tagsView): 创建tags数据源

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -5,7 +5,45 @@
 </template>
 
 <script setup>
-import {} from 'vue'
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { isTags } from '@/utils/tags'
+import { useStore } from 'vuex'
+import { generateTitle } from '@/utils/i18n'
+
+// 生成title
+const getTitle = (route) => {
+  let title = ''
+  if (!route.meta) {
+    const pathArr = route.path.split('/')
+    title = pathArr[pathArr.length - 1]
+  } else {
+    title = generateTitle(route.meta.title)
+  }
+  return title
+}
+
+const store = useStore()
+const route = useRoute()
+watch(
+  route,
+  (to, from) => {
+    if (!isTags(to.path)) return
+    const { fullPath, meta, name, params, path, query } = to
+    store.commit('app/addTagsViewList', {
+      fullPath,
+      meta,
+      name,
+      params,
+      path,
+      query,
+      title: getTitle(to)
+    })
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
 <style lang="scss" scoped>
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,11 +1,12 @@
-import { LANG } from '@/constant'
+import { LANG, TAGS_VIEW } from '@/constant'
 import { getItem, setItem } from '@/utils/storage'
 
 export default {
   namespaced: true,
   state: () => ({
     sidebarOpened: true,
-    language: getItem(LANG) || 'zh'
+    language: getItem(LANG) || 'zh',
+    tagsViewList: getItem(TAGS_VIEW) || []
   }),
   mutations: {
     triggerSidebarOpened(state) {
@@ -17,6 +18,19 @@ export default {
     setLanguage(state, lang) {
       setItem(LANG, lang)
       state.language = lang
+    },
+    /**
+     * 添加 tags
+     */
+    addTagsViewList(state, tag) {
+      const isFind = state.tagsViewList.find((item) => {
+        return item.path === tag.path
+      })
+      // 处理重复
+      if (!isFind) {
+        state.tagsViewList.push(tag)
+        setItem(TAGS_VIEW, state.tagsViewList)
+      }
     }
   },
   actions: {}
