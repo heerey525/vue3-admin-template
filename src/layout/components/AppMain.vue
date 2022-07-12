0dcf30fbd7535d4bfb9e39700741e7ac10a3be9e
heerey525 <190182347@qq.com>
2022-07-12 22:28:23 +0800

    feat(TagsView): tagsView国际化

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -9,7 +9,7 @@ import { watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { isTags } from '@/utils/tags'
 import { useStore } from 'vuex'
-import { generateTitle } from '@/utils/i18n'
+import { generateTitle, watchSwitchLang } from '@/utils/i18n'
 
 // 生成title
 const getTitle = (route) => {
@@ -44,6 +44,20 @@ watch(
     immediate: true
   }
 )
+/**
+ * 国际化 tags
+ */
+watchSwitchLang(() => {
+  store.getters.tagsViewList.forEach((route, index) => {
+    store.commit('app/changeTagsView', {
+      index,
+      tag: {
+        ...route,
+        title: getTitle(route)
+      }
+    })
+  })
+})
 </script>
 
 <style lang="scss" scoped>
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -31,6 +31,13 @@ export default {
         state.tagsViewList.push(tag)
         setItem(TAGS_VIEW, state.tagsViewList)
       }
+    },
+    /**
+     * 为指定的 tag 修改 title
+     */
+    changeTagsView(state, { index, tag }) {
+      state.tagsViewList[index] = tag
+      setItem(TAGS_VIEW, state.tagsViewList)
     }
   },
   actions: {}
