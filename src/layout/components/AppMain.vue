e1d87622c1beb94c1698276b5e93d15acd0f4604
heerey525 <190182347@qq.com>
2022-07-12 23:20:13 +0800

    feat(动态过渡): 基于路由的动态过渡

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app-main">
-    <router-view></router-view>
+    <router-view v-slot="{ Component, route }">
+      <transition name="fade-transform" mode="out-in">
+        <keep-alive>
+          <component :is="Component" :key="route.path" />
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -62,11 +68,11 @@ watchSwitchLang(() => {
 
 <style lang="scss" scoped>
 .app-main {
-  min-height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px - 43px);
   width: 100%;
   position: relative;
   overflow: hidden;
-  padding: 61px 20px 20px 20px;
+  padding: 104px 20px 20px 20px;
   box-sizing: border-box;
 }
 </style>
