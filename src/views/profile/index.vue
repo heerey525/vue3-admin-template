e67c07145f65e8666f399f6a9c7fa0ec6fa17846
heerey525 <190182347@qq.com>
2022-07-13 23:14:55 +0800

    feat(个人中心): 个人中心模块基本布局

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,25 +1,42 @@
 <template>
-  <div>
-    <div class="">个人中心</div>
-    <el-pagination
-      :page-size="100"
-      layout="total, prev, pager, next"
-      :total="1000"
-    />
-    <el-row class="mb-4">
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-      <el-button>中文</el-button>
+  <div class="my-container">
+    <el-row>
+      <el-col :span="6">
+        <project-card class="user-card"></project-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <feature />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <chapter />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <author />
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import ProjectCard from './components/ProjectCard.vue'
+import Chapter from './components/Chapter.vue'
+import Feature from './components/Feature.vue'
+import Author from './components/Author.vue'
+import { ref } from 'vue'
+
+const activeName = ref('feature')
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.my-container {
+  .user-card {
+    margin-right: 20px;
+  }
+}
+</style>
